cc91be5543944e19bbbc8e61bc804b2f256303f4
snowsphere <info@snowsphere.net>
2023-06-12 04:56:01 +0900

    feat: add nuxtlabs-ui

---
+++ packages/nuxt3/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { User } from '../../api/seeds'
+
+const res = await fetch('http://localhost:8000/users')
+const resJson: User[] = await res.json()
+</script>
+
+<template>
+  <UContainer class="mt-4">
+    <UCard>
+      <UTable :rows="resJson"></UTable>
+    </UCard>
+  </UContainer>
+</template>
